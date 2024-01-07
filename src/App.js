b63f111be9b9f8d5475592cc09c6e0bc3ca73e5e
alihernandez <ali.hernandez9two@gmail.com>
2024-01-06 17:43:33 -0700

    state example

--- src/App.js
+++ src/App.js
@@ -3,22 +3,24 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super(); //Calls underlying constructor method
+
+    this.state = {
+      name: 'Beck'
+    }
+  }
   render() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hello {this.state.name}.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <button onClick={() => {
+          this.setState({name: 'Ali'})
+        }}>Change Name</button>
       </header>
     </div>
   );
